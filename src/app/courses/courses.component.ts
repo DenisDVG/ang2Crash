e5863f5ec85a972805c80dcb8b60c03df35ba084
DenisGromenkoV <156789qw@gmail.com>
2020-11-05 21:56:43 +0200

    events filter

--- src/app/courses/courses.component.ts
+++ src/app/courses/courses.component.ts
@@ -8,9 +8,22 @@ import { CoursesService } from '../courses.service';
 })
 export class CoursesComponent implements OnInit {
   courses: string[];
+  title = 'List of courses';
+  imageUrl = 'http://lorempixel.com/400/200';
+  colSpan = 2;
+  isActive = true;
   constructor(private coursesService: CoursesService) {
     this.courses = coursesService.getCourses();
   }
-
+  onSave($event): void {
+    $event.stopPropagation();
+    console.log('Btn onSave was clicked', $event);
+  }
+  onDivAfterSave(): void {
+    console.log('onDivAfterSave');
+  }
+  onKeyUpFilterEnter(): void {
+      console.log('Enter was pressed');
+  }
   ngOnInit(): void {}
 }
